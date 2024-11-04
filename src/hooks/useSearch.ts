9fb722b3a0a5118f41ee59e3e400ceabbee9a44b
dongjang <jdwls27@naver.com>
2024-11-04 18:23:37 +0900

    useCalendarTest 작업 중

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -4,12 +4,14 @@ import { Event } from '../types';
 import { getFilteredEvents } from '../utils/eventUtils';
 
 export const useSearch = (events: Event[], currentDate: Date, view: 'week' | 'month') => {
+  console.log('events', events);
   const [searchTerm, setSearchTerm] = useState('');
 
   const filteredEvents = useMemo(() => {
     return getFilteredEvents(events, searchTerm, currentDate, view);
   }, [events, searchTerm, currentDate, view]);
 
+  console.log(filteredEvents);
   return {
     searchTerm,
     setSearchTerm,
