e204df45ec6a785d00258e8450a7d75617ef3508
dongjang <jdwls27@naver.com>
2024-11-06 17:45:52 +0900

    eventSave 수정

---
+++ src/components/eventsSave/EventsSaveTitle.tsx
@@ -0,0 +1,23 @@
+import { FormControl, FormLabel, Input } from '@chakra-ui/react';
+import React from 'react';
+
+interface EventsSaveTitleProps {
+  title: string;
+  setTitle: (value: React.SetStateAction<string>) => void;
+}
+
+const EventsSaveTitle = ({ title, setTitle }: EventsSaveTitleProps) => {
+  function handleTitleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setTitle(event.target.value);
+  }
+  return (
+    <>
+      <FormControl>
+        <FormLabel>제목</FormLabel>
+        <Input value={title} onChange={handleTitleChange} />
+      </FormControl>
+    </>
+  );
+};
+
+export default EventsSaveTitle;
