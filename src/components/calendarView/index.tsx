dbb74b8b3bff3ac150162e52641bfdf59343a825
JANG DONG JIN <jdwls27@naver.com>
2024-11-07 15:41:57 +0900

    calendarView 완성

--- src/components/calendarView/index.tsx
+++ src/components/calendarView/index.tsx
@@ -15,6 +15,8 @@ interface CalendarViewProps {
   holidays: { [key: string]: string };
   notifiedEvents: string[];
   filteredEvents: Event[];
+  resetForm: () => void;
+  setSearchTerm: React.Dispatch<React.SetStateAction<string>>;
 }
 
 const CalendarView = ({
@@ -26,51 +28,60 @@ const CalendarView = ({
   notifiedEvents,
   filteredEvents,
   holidays,
+  resetForm,
+  setSearchTerm,
 }: CalendarViewProps) => {
+  function handleViewChange(e: React.ChangeEvent<HTMLSelectElement>) {
+    setView(e.target.value as 'week' | 'month');
+    resetForm();
+    setSearchTerm('');
+  }
+
+  function handleNavigateChange(btnType: string) {
+    navigate(btnType as 'prev' | 'next');
+    resetForm();
+    setSearchTerm('');
+  }
+
   return (
-    <>
-      <VStack flex={1} spacing={5} align="stretch">
-        <Heading>일정 보기</Heading>
+    <VStack flex={1} spacing={5} align="stretch">
+      <Heading>일정 보기</Heading>
 
-        <HStack mx="auto" justifyContent="space-between">
-          <IconButton
-            aria-label="Previous"
-            icon={<ChevronLeftIcon />}
-            onClick={() => navigate('prev')}
-          />
-          <Select
-            aria-label="view"
-            value={view}
-            onChange={(e) => setView(e.target.value as 'week' | 'month')}
-          >
-            <option value="week">Week</option>
-            <option value="month">Month</option>
-          </Select>
-          <IconButton
-            aria-label="Next"
-            icon={<ChevronRightIcon />}
-            onClick={() => navigate('next')}
-          />
-        </HStack>
+      <HStack mx="auto" justifyContent="space-between">
+        <IconButton
+          aria-label="Previous"
+          icon={<ChevronLeftIcon />}
+          onClick={() => handleNavigateChange('prev')}
+        />
+        <Select aria-label="view" value={view} onChange={handleViewChange}>
+          <option value="week">Week</option>
+          <option value="month">Month</option>
+        </Select>
 
-        {view === 'week' && (
-          <CalendarWeekView
-            currentDate={currentDate}
-            events={events}
-            view={view}
-            notifiedEvents={notifiedEvents}
-          />
-        )}
-        {view === 'month' && (
-          <CalendarMonthView
-            currentDate={currentDate}
-            filteredEvents={filteredEvents}
-            notifiedEvents={notifiedEvents}
-            holidays={holidays}
-          />
-        )}
-      </VStack>
-    </>
+        <IconButton
+          aria-label="Next"
+          icon={<ChevronRightIcon />}
+          onClick={() => handleNavigateChange('next')}
+        />
+      </HStack>
+
+      {view === 'week' && (
+        <CalendarWeekView
+          currentDate={currentDate}
+          events={events}
+          view={view}
+          notifiedEvents={notifiedEvents}
+        />
+      )}
+      {view === 'month' && (
+        <CalendarMonthView
+          currentDate={currentDate}
+          filteredEvents={filteredEvents}
+          notifiedEvents={notifiedEvents}
+          holidays={holidays}
+        />
+      )}
+    </VStack>
   );
 };
 
